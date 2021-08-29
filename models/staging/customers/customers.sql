3bfd676d7b4883fc8bc4b92a8d6ce08f4e9a89bb
Nick Bosma <nick.bosma@educationperfect.com>
2021-08-29 14:52:38 +0000

    actual footage of me fixing the problem

--- models/staging/customers/customers.sql
+++ models/staging/customers/customers.sql
@@ -1,24 +1,23 @@
 {{ config(materialized='table') }}
 
 with customers as (
-	select * from  {{ source ('coffee_shop', 'customers') }}
+  select * from  {{ source ('coffee_shop', 'customers') }}
 ),
 
 orders as (
-	select * from {{source ('coffee_shop', 'orders') }},
+  select * from {{source ('coffee_shop', 'orders') }}
 ),
 
-transformed as
-
-select
-    customers.id as customer_id,
-    customers.name,
-    customers.email,
-    min(orders.created_at) as first_order_at,
-    coalesce(count(distinct orders.id),0) as number_of_orders
-from customers
-left outer join orders on customers.id = orders.customer.id
-group by 1,2,3
+transformed as (
+  select
+     customers.id as customer_id,
+     customers.name,
+     customers.email,
+     min(orders.created_at) as first_order_at,
+     coalesce(count(distinct orders.id),0) as number_of_orders
+  from customers
+  left outer join orders on customers.id = orders.customer.id
+  group by 1,2,3
 )
 
 select * from transformed

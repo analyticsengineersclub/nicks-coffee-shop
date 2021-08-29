736cd0dfe431b3efd77c9ef803ce73da3e1091fb
Nick Bosma <nick.bosma@educationperfect.com>
2021-08-29 14:47:47 +0000

    cte-ify model

--- models/staging/customers/customers.sql
+++ models/staging/customers/customers.sql
@@ -1,12 +1,22 @@
 {{ config(materialized='table') }}
 
+with customers as (
+	select * from  {{ source ('coffee_shop', 'customers') }}
+),
+
+orders as (
+	select * from {{source ('coffee_shop', 'orders') }},
+),
+
+transformed as
+
 select
     customers.id as customer_id,
     customers.name,
     customers.email,
     min(orders.created_at) as first_order_at,
     coalesce(count(distinct orders.id),0) as number_of_orders
-from `analytics-engineers-club.coffee_shop.orders` as orders
-left outer join `analytics-engineers-club.coffee_shop.customers` as customers on orders.customer_id = customers.id
+from customers
+left outer join orders on customers.id = orders.customer.id
 group by 1,2,3
 order by first_order_at

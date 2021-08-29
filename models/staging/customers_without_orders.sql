457c1a4ee21ce512260bcb53ae99050683be261c
Nick Bosma <nick.bosma@educationperfect.com>
2021-08-29 10:28:29 +0000

    add trasformative cte to imrpove model

--- models/staging/customers_without_orders.sql
+++ models/staging/customers_without_orders.sql
@@ -1,6 +1,10 @@
 with src as (
   select * from {{ ref('customers') }}
+),
+
+transformed as (
+  select * from src
+  where number_of_orders = 0
 )
 
-select * from src
-where number_of_orders = 0
+select * from transformed

a0c64974d319fecc3fd700bc1fbe6c57b34b05d5
Nick Bosma <nick.bosma@educationperfect.com>
2021-09-05 05:19:25 +0000

    separate customers and orders more effectively

--- models/staging/customers/customers.sql
+++ models/staging/customers/customers.sql
@@ -4,20 +4,12 @@ with customers as (
   select * from  {{ source ('coffee_shop', 'customers') }}
 ),
 
-orders as (
-  select * from {{source ('coffee_shop', 'orders') }}
-),
-
 transformed as (
   select
      customers.id as customer_id,
-     customers.name,
-     customers.email,
-     min(orders.created_at) as first_order_at,
-     coalesce(count(distinct orders.id),0) as number_of_orders
+     customers.name as customer_name,
+     customers.email as customer_email
   from customers
-  left outer join orders on customers.id = orders.customer_id
-  group by 1,2,3
 )
 
 select * from transformed
